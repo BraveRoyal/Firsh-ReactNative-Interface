c1159eaeb6fb394fdbd437aac48bd4eac5309918
BraveRoyal <braveroyal2009@gmail.com>
2021-01-14 08:19:49 -0300

    Aula 2: Scroll De Novidades

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { useFonts, Montserrat_400Regular, Montserrat_500Medium, Montserrat_700Bold} from '@expo-google-fonts/montserrat';
+import { useFonts, Montserrat_400Regular, Montserrat_500Medium, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
 import Routes from './src/router';
 
